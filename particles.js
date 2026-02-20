db58eebcb6364132f211dcf82a36da2248933d65
Utkarsh Bajpai <mailtoutkarsh_bajpai@rediffmail.com>
2026-02-20 23:49:28 +0530

    Initialize particles.js with configuration

--- particles.js
+++ particles.js
@@ -1,1 +1,8 @@
-
+particlesJS.load('particles-js', {
+  particles: {
+    number: { value: 80 },
+    size: { value: 3 },
+    color: { value: "#facc15" },
+    line_linked: { enable: true, color: "#facc15" }
+  }
+});
