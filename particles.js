2dbbc5f803d24ac57844d86c16a2dce4b1d8b562
Utkarsh Bajpai <mailtoutkarsh_bajpai@rediffmail.com>
2026-02-21 00:25:50 +0530

    Delete particles.js

--- particles.js
+++
@@ -1,8 +0,0 @@
-particlesJS.load('particles-js', {
-  particles: {
-    number: { value: 80 },
-    size: { value: 3 },
-    color: { value: "#facc15" },
-    line_linked: { enable: true, color: "#facc15" }
-  }
-});
